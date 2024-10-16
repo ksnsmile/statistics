8e56a39759ce850d6391ddf4fbebd2b2c26e3e96
DESKTOP-9JIUSGJ\ksn71 <ksn7141@naver.com>
2024-10-16 18:45:55 +0900

    no message

--- 2회_상관분석/2상관분석.R
+++ 2회_상관분석/2상관분석.R
@@ -24,6 +24,8 @@ i <- lower.tri(Cl)
 cor(cbind(P = Cl[i], S = clS[i], K = clK[i]))
 
 # 패키지 "corrgram"설치
+install.packages("corrgram")
+
 library(corrgram)
 corrgram(cor(longley), main = "상관계수의 시각화", type="corr",cor.method = "pearson", upper.panel=panel.conf)
 
@@ -33,7 +35,7 @@ cov( cbind ( c(1,2,3,4), c(2,3,6,8), c(3,5,8,10) ))
 
 cov( cbind ( c(1,2,3,4), c(2,3,6,8), c(3,5,8,10)), use="everything")
 
-cov( cbind ( c(1,2,3,4), c(2,3,6,8), c(3,5,8,10)), use="all")
+cov( cbind ( c(1,2,3,4), c(2,3,6,8), c(3,5,8,10)), use="all") #결측치가 있어도 그냥 계산
 
 
 cov( cbind ( c(NA,2,3,4), c(2,3,6,8), c(3,5,8,10)), use="everything")
@@ -48,6 +50,10 @@ cov( cbind ( c(2,3,4), c(3,6,8), c(5,8,10)), use="all")
 
 cov( cbind ( c(NA,2,3,4), c(2,3,6,8), c(3,5,8,10)), use="pairwise")
 
+# "complete": 모든 변수에서 NA가 없는 행만 사용하여 계산.
+# "everything": NA가 있는 경우 결과는 NA가 되며, NA가 없을 때는 모든 데이터를 사용.
+# "all": NA가 있는 경우에도 가능한 모든 데이터를 사용.
+# "pairwise": 각 변수 쌍에서 NA가 없는 데이터만 사용하여 개별적으로 계산.
 
 #상관분석
 x <- c(44.4, 45.9, 41.9, 53.3, 44.7, 44.1, 50.7, 45.2, 60.1)
--- 2회_상관분석/R_03_3_이변량정규분포_dynamic graph.R
+++ 2회_상관분석/R_03_3_이변량정규분포_dynamic graph.R
@@ -1,7 +1,7 @@
 ## 좌표의 범위 설정 
-X <- seq(-3, 3, .01) -> Y
+X <- seq(-3, 3, .01) -> Y # -3부터 3까지 0.1의 간격을 가지고 설정
 
-#install.packages("mvtnorm")
+install.packages("mvtnorm")
 library(mvtnorm)
 
 ## x 와 y 의 평면 좌표값 설정
@@ -15,7 +15,7 @@ Sigma =matrix(c(Sigma_1,Rho,Rho,Sigma_2),2,2);
 ##이변량정규분포 함수값 결정
 Z <- dmvnorm(XY, mean=Mean, sigma=Sigma)
 
-#install.packages("rgl")
+install.packages("rgl")
 library(rgl)
 
 persp3d(X,Y,matrix(Z,length(X),length(Y)),smooth=T,color=rgb(0.7,0.5,0.2, 0.5),
